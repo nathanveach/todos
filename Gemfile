34e86a7edd899ebbf5be8851701c8b6a392bdebe
nathanveach <nathanbveach@gmail.com>
2020-04-22 13:27:02 -0700

    Allow a user to create a todo

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ group :development, :test do
 end
 
 group :test do
-	gem 'capybara', '~> 2.3.0'
+	gem 'capybara'
 end
 
 group :development do
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+
+feature "User creates todo" do  
+	scenario "Succesfully" do
+		visit root_path
+
+		click_on "Add a new todo"
+		fill_in "Title", with: "Buy milk"
+		click_on "Submit"
+
+		expect(page).to have_content "Buy milk"
+
+	end
+end
